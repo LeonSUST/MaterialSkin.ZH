719d8bc068da3948a9b95685a38c421801432e60
LeonSUST <43171315@qq.com>
2022-11-04 16:08:37 +0800

    first commit

---
+++ MaterialSkin/Controls/MaterialProgressBar.cs
@@ -0,0 +1,39 @@
+﻿namespace MaterialSkin.Controls
+{
+    using System.ComponentModel;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    public class MaterialProgressBar : ProgressBar, IMaterialControl
+    {
+        public MaterialProgressBar()
+        {
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        [Browsable(false)]
+        public int Depth { get; set; }
+
+        [Browsable(false)]
+        public MaterialSkinManager SkinManager => MaterialSkinManager.Instance;
+
+        [Browsable(false)]
+        public MouseState MouseState { get; set; }
+
+        protected override void SetBoundsCore(int x, int y, int width, int height, BoundsSpecified specified)
+        {
+            base.SetBoundsCore(x, y, width, 5, specified);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var doneProgress = (int)(Width * ((double)Value / Maximum));
+            e.Graphics.FillRectangle(Enabled ? 
+                SkinManager.ColorScheme.PrimaryBrush :
+                new SolidBrush(DrawHelper.BlendColor(SkinManager.ColorScheme.PrimaryColor, SkinManager.SwitchOffDisabledThumbColor, 197)),
+                0, 0, doneProgress, Height);
+            e.Graphics.FillRectangle(SkinManager.BackgroundFocusBrush, doneProgress, 0, Width - doneProgress, Height);
+        }
+    }
+}
---
+++ MaterialSkinExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MaterialSkinExample
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(true);
+            Application.Run(new MainForm());
+        }
+    }
+}
